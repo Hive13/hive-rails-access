feb700e1354c2a4eceb800cbf9438947754cb196
Ian Wilson <me@ianwilson.org>
2013-06-10 19:13:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hive13RailsAccess::Application.config.secret_token = '99900bb05df476fe42a43c182424159c15ac9a7b261e8d633157ed86790caa44b57975958f6d610a3f30491dd6d3f98ec0dee292052f044fa454a29fef93d8d4'
