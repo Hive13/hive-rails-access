5a383a4a52765d1322d7e1bb62b1d60aadf8c081
Ian Wilson <me@ianwilson.org>
2013-08-01 17:37:30 -0400

    Needed to actually look at my migrations there...

--- db/migrate/20130801152553_add_doorimage_and_delete_bs_from_members.rb
+++ db/migrate/20130801152553_add_doorimage_and_delete_bs_from_members.rb
@@ -4,7 +4,7 @@ class AddDoorimageAndDeleteBsFromMembers < ActiveRecord::Migration
     remove_column :members, :checkin_picture_file_name
     remove_column :members, :checkin_picture_content_type
     remove_column :members, :checkin_picture_file_size
-    remove_column :members, :checkin_picture_uploaded_at
+    remove_column :members, :checkin_picture_updated_at
     remove_column :members, :avatar_file_name
     remove_column :members, :avatar_content_type
     remove_column :members, :avatar_file_size
